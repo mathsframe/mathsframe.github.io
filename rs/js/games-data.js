0e6e9f19425468959e4e64bc4e381d331111c12c
Haleyschool <taikhoanlenvoi@gmail.com>
2025-02-22 16:50:31 +0700

    89

--- rs/js/games-data.js
+++ rs/js/games-data.js
@@ -2,34 +2,510 @@ const gamesData = {
   getAll: function () {
     return [
       {
-        title: "math duck",
-        image: "https://mathsframe.github.io/alllogo/math-duck/logo.webp",
-        url: "./games/math-duck.html",
-        isHot: true,
+        title: "adventure drivers",
+        url: "adventure-drivers.html",
+        image:
+          "https://mathsframe.github.io/alllogo/adventure-drivers/logo.png",
       },
       {
-        title: "fast math",
-        image: "https://mathsframe.github.io/alllogo/fast-math/logo.png",
-        url: "./games/fast-math.html",
-        isHot: true,
+        title: "car drawings",
+        url: "car-drawing.html",
+        image: "https://mathsframe.github.io/alllogo/car-drawing/logo.jpeg",
       },
       {
-        title: "1v1 lol",
-        image: "https://mathsframe.github.io/alllogo/1v1-lol/logo.png",
-        url: "./games/1v1-lol.html",
-        isHot: true,
+        title: "cars simulator",
+        url: "cars-simulator.html",
+        image: "https://mathsframe.github.io/alllogo/cars-simulator/logo.png",
       },
       {
         title: "drift hunters",
+        url: "drift-hunters.html",
         image: "https://mathsframe.github.io/alllogo/drift-hunters/logo.png",
-        url: "./games/drift-hunters.html",
-        isHot: true,
+      },
+      {
+        title: "drive mad",
+        url: "drive-mad.html",
+        image: "https://mathsframe.github.io/alllogo/drive-mad/logo.jpg",
       },
       {
         title: "eggy car",
+        url: "eggy-car.html",
         image: "https://mathsframe.github.io/alllogo/eggy-car/logo.jpeg",
-        url: "./games/eggy-car.html",
-        isHot: false,
+      },
+      {
+        title: "hilldraw",
+        url: "hilldraw.html",
+        image: "https://mathsframe.github.io/alllogo/hilldraw/logo.png",
+      },
+      {
+        title: "jellytruck",
+        url: "jellytruck.html",
+        image: "https://mathsframe.github.io/alllogo/jellytruck/logo.png",
+      },
+      {
+        title: "madalin stunt cars 2",
+        url: "madalin-stunt-cars-2.html",
+        image:
+          "https://mathsframe.github.io/alllogo/madalin-stunt-cars-2/logo.jpg",
+      },
+      {
+        title: "madalin stunt cars 3",
+        url: "madalin-stunt-cars-3.html",
+        image:
+          "https://mathsframe.github.io/alllogo/madalin-stunt-cars-3/logo.jpg",
+      },
+      {
+        title: "bitlife",
+        url: "bitlife.html",
+        image: "https://mathsframe.github.io/alllogo/bitlife/logo.png",
+      },
+      {
+        title: "capybara clicker",
+        url: "capybara-clicker.html",
+        image: "https://mathsframe.github.io/alllogo/capybara-clicker/logo.png",
+      },
+      {
+        title: "chess online",
+        url: "chess-online.html",
+        image: "https://mathsframe.github.io/alllogo/chess-online/logo.png",
+      },
+      {
+        title: "cookie clicker",
+        url: "cookie-clicker.html",
+        image: "https://mathsframe.github.io/alllogo/cookie-clicker/logo.png",
+      },
+      {
+        title: "foosball",
+        url: "foosball.html",
+        image: "https://mathsframe.github.io/alllogo/foosball/logo.jfif",
+      },
+      {
+        title: "geometry neon dash world two",
+        url: "geometryneondashworldtwo.html",
+        image:
+          "https://mathsframe.github.io/alllogo/geometryneondashworldtwo/logo.png",
+      },
+      {
+        title: "zombie shooter",
+        url: "zombie-shooter.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/zombie-shooter/logo.png",
+      },
+      {
+        title: "mace random",
+        url: "mace-random.html",
+        image: "https://mathsframe.github.io/alllogo/mace-random/logo.png",
+      },
+      {
+        title: "basket and ball",
+        url: "basket-and-ball.html",
+        image: "https://mathsframe.github.io/alllogo/basket-and-ball/logo.jpg",
+      },
+      {
+        title: "christmas fishing io",
+        url: "christmasfishingio.html",
+        image:
+          "https://mathsframe.github.io/alllogo/christmasfishingio/logo.png",
+      },
+      {
+        title: "mini golf",
+        url: "edge-surf.html",
+        image: "https://mathsframe.github.io/alllogo/mini-golf/logo.png",
+      },
+      {
+        title: "Soccer Heads",
+        url: "SoccerHeads.html",
+        image: "https://mathsframe.github.io/alllogo/1hls/SoccerHeads/logo.png",
+      },
+      {
+        title: "moto x3m pool party",
+        url: "moto-x3m-pool-party.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/moto-x3m-pool-party/logo.png",
+      },
+      {
+        title: "backrooms 2d",
+        url: "Flappy-ZigZag.html",
+        image: "https://mathsframe.github.io/alllogo/backrooms-2d/logo.jpg",
+      },
+      {
+        title: "FNAF1",
+        url: "FNAF1.html",
+        image: "https://mathsframe.github.io/alllogo/FNAF1/logo.jpg",
+      },
+      {
+        title: "FNAF2 hack",
+        url: "FNAF2-hack.html",
+        image: "https://mathsframe.github.io/alllogo/FNAF2-hack/logo.png",
+      },
+      {
+        title: "FNAF2",
+        url: "FNAF2.html",
+        image: "https://mathsframe.github.io/alllogo/FNAF2/logo.png",
+      },
+      {
+        title: "FNAF3",
+        url: "FNAF3.html",
+        image: "https://mathsframe.github.io/alllogo/FNAF3/logo.jfif",
+      },
+      {
+        title: "FNAF4",
+        url: "FNAF4.html",
+        image: "https://mathsframe.github.io/alllogo/FNAF4/logo.png",
+      },
+      {
+        title: "Among Us",
+        url: "among-us.html",
+        image: "https://mathsframe.github.io/alllogo/among-us/logo.png",
+      },
+      {
+        title: "Awesome Tanks 2",
+        url: "awesometanks2.html",
+        image: "https://mathsframe.github.io/alllogo/awesometanks2/logo.jpg",
+      },
+      {
+        title: "10 Minutes Till Dawn",
+        url: "10-minutes-till-dawn.html",
+        image:
+          "https://mathsframe.github.io/alllogo/10-minutes-till-dawn/logo.png",
+      },
+      {
+        title: "2 3 4 Player Games",
+        url: "2-3-4-player-games.html",
+        image:
+          "https://mathsframe.github.io/alllogo/2-3-4-player-games/logo.png",
+      },
+      {
+        title: "Bejeweled",
+        url: "bejeweled.html",
+        image: "https://mathsframe.github.io/alllogo/bejeweled/logo.jpg",
+      },
+      {
+        title: "Chess Online",
+        url: "chess-online.html",
+        image: "https://mathsframe.github.io/alllogo/chess-online/logo.png",
+      },
+      {
+        title: "Hacker Type",
+        url: "hackertype.html",
+        image: "https://mathsframe.github.io/alllogo/hackertype/logo.png",
+      },
+      {
+        title: "Mineclone",
+        url: "mineclone.html",
+        image: "https://mathsframe.github.io/alllogo/mineclone/logo.png",
+      },
+      {
+        title: "8 Ball Pool",
+        url: "8-ball-pool.html",
+        image: "https://mathsframe.github.io/alllogo/8-ball-pool/logo.png",
+      },
+      {
+        title: "Math Duck",
+        url: "math-duck.html",
+        image: "https://mathsframe.github.io/alllogo/math-duck/logo.webp",
+      },
+      {
+        title: "Monkey Mart",
+        url: "monkey-mart.html",
+        image: "https://mathsframe.github.io/alllogo/monkey-mart/logo.png",
+      },
+      {
+        title: "Bacon May Die",
+        url: "bacon-may-die.html",
+        image: "https://mathsframe.github.io/alllogo/bacon-may-die/logo.png",
+      },
+      {
+        title: "Fast Math",
+        url: "fast-math.html",
+        image: "https://mathsframe.github.io/alllogo/fast-math/logo.png",
+      },
+      {
+        title: "Doge 2048",
+        url: "doge2048.html",
+        image: "https://mathsframe.github.io/alllogo/doge2048/logo.jpeg",
+      },
+      {
+        title: "Bouncing Hell",
+        url: "bouncinghell.html",
+        image: "https://mathsframe.github.io/alllogo/bouncinghell/logo.png",
+      },
+      {
+        title: "Helios",
+        url: "helios.html",
+        image: "https://mathsframe.github.io/alllogo/helios/logo.jfif",
+      },
+      {
+        title: "Among Dash",
+        url: "among-dash.html",
+        image: "https://mathsframe.github.io/alllogo/among-dash/logo.png",
+      },
+      {
+        title: "Aqua Park",
+        url: "aqua-park.html",
+        image: "https://mathsframe.github.io/alllogo/aqua-park/logo.png",
+      },
+      {
+        title: "Ball2",
+        url: "ball2.html",
+        image: "https://mathsframe.github.io/alllogo/ball2/logo.jpg",
+      },
+      {
+        title: "Geometry Rash",
+        url: "geometryrash.html",
+        image: "https://mathsframe.github.io/alllogo/geometryrash/logo.jfif",
+      },
+      {
+        title: "Death Run",
+        url: "deathrun.html",
+        image: "https://mathsframe.github.io/alllogo/deathrun/logo.jpg",
+      },
+      {
+        title: "GeoDash2",
+        url: "geodash2.html",
+        image: "https://mathsframe.github.io/alllogo/geodash2/logo.png",
+      },
+      {
+        title: "Geometry Neon Dash World Two",
+        url: "geometryneondashworldtwo.html",
+        image:
+          "https://mathsframe.github.io/alllogo/geometryneondashworldtwo/logo.png",
+      },
+      {
+        title: "Mario",
+        url: "mario.html",
+        image: "https://mathsframe.github.io/alllogo/mario/logo.jfif",
+      },
+      {
+        title: "OVO",
+        url: "ovo.html",
+        image: "https://mathsframe.github.io/alllogo/ovo/logo.png",
+      },
+      {
+        title: "Pacman 3D",
+        url: "pacman3d.html",
+        image: "https://mathsframe.github.io/alllogo/pacman3d/logo.jpg",
+      },
+      {
+        title: "Flappy ZigZag",
+        url: "Flappy-ZigZag.html",
+        image: "https://mathsframe.github.io/alllogo/Flappy-ZigZag/logo.png",
+      },
+      {
+        title: "Backrooms 2D",
+        url: "backrooms-2d.html",
+        image: "https://mathsframe.github.io/alllogo/backrooms-2d/logo.jpg",
+      },
+      {
+        title: "Baldi's Basics Field Trip Camping Remake",
+        url: "baldisbasicsfieldtripcampingremake.html",
+        image:
+          "https://mathsframe.github.io/alllogo/baldisbasicsfieldtripcampingremake/logo.png",
+      },
+      {
+        title: "1v1 lol",
+        url: "1v1-lol.html",
+        image: "https://mathsframe.github.io/alllogo/1v1-lol/logo.png",
+      },
+      {
+        title: "Archery Random",
+        url: "archery-random.html",
+        image: "https://mathsframe.github.io/alllogo/archery-random/logo.jpeg",
+      },
+      {
+        title: "Axe Random",
+        url: "axe-random.html",
+        image: "https://mathsframe.github.io/alllogo/axe-random/logo.png",
+      },
+      {
+        title: "Funny Shooter 2",
+        url: "funny-shooter-2.html",
+        image: "https://mathsframe.github.io/alllogo/funny-shooter-2/logo.jpeg",
+      },
+      {
+        title: "Funny Shooter",
+        url: "funny-shooter.html",
+        image: "https://mathsframe.github.io/alllogo/funny-shooter/logo.jpeg",
+      },
+      {
+        title: "Getaway Shootout",
+        url: "getawayshootout.html",
+        image: "https://mathsframe.github.io/alllogo/getawayshootout/logo.jpg",
+      },
+      {
+        title: "Gun Random",
+        url: "gun-random.html",
+        image: "https://mathsframe.github.io/alllogo/gun-random/logo.png",
+      },
+      {
+        title: "Justfall",
+        url: "justfall.html",
+        image: "https://mathsframe.github.io/alllogo/justfall/logo.jpg",
+      },
+      {
+        title: "Mr Bullet",
+        url: "mr-bullet.html",
+        image: "https://mathsframe.github.io/alllogo/mr-bullet/logo.png",
+      },
+      {
+        title: "Pacman FPS",
+        url: "pacmanfps.html",
+        image: "https://mathsframe.github.io/alllogo/pacmanfps/logo.jpg",
+      },
+      {
+        title: "Galaga Mini",
+        url: "GalagaMini.html",
+        image: "https://mathsframe.github.io/alllogo/GalagaMini/logo.png",
+      },
+      {
+        title: "Antiterroristrush 3",
+        url: "antiterroristrush3.html",
+        image:
+          "https://mathsframe.github.io/alllogo/antiterroristrush3/logo.png",
+      },
+      {
+        title: "Archer Master 3D Castle Defense",
+        url: "archer-master-3d-castle-defense.html",
+        image:
+          "https://mathsframe.github.io/alllogo/archer-master-3d-castle-defense/logo.png",
+      },
+      {
+        title: "Blockpost",
+        url: "blockpost.html",
+        image: "https://mathsframe.github.io/alllogo/blockpost/logo.png",
+      },
+      {
+        title: "Knife Hit",
+        url: "knife-hit.html",
+        image: "https://mathsframe.github.io/alllogo/knife-hit/logo.png",
+      },
+      {
+        title: "Lethal Sniper 3D Army Soldier",
+        url: "lethal-sniper-3d-army-soldier.html",
+        image:
+          "https://mathsframe.github.io/alllogo/lethal-sniper-3d-army-soldier/logo.png",
+      },
+      {
+        title: "Mace Random",
+        url: "mace-random.html",
+        image: "https://mathsframe.github.io/alllogo/mace-random/logo.png",
+      },
+      {
+        title: "Time Shooter",
+        url: "Time-Shooter.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/Time-Shooter/logo.png",
+      },
+      {
+        title: "Zombie Shooter",
+        url: "zombie-shooter.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/zombie-shooter/logo.png",
+      },
+      {
+        title: "Basket Random",
+        url: "basket-random.html",
+        image: "https://mathsframe.github.io/alllogo/basket-random/logo.jpeg",
+      },
+      {
+        title: "Boxing Random",
+        url: "boxing-random.html",
+        image: "https://mathsframe.github.io/alllogo/boxing-random/logo.png",
+      },
+      {
+        title: "Fishing Game",
+        url: "fishing-game.html",
+        image: "https://mathsframe.github.io/alllogo/fishing-game/logo.png",
+      },
+      {
+        title: "Foosball",
+        url: "foosball.html",
+        image: "https://mathsframe.github.io/alllogo/foosball/logo.jfif",
+      },
+      {
+        title: "Friday Night Funkin",
+        url: "fridaynightfunkin.html",
+        image:
+          "https://mathsframe.github.io/alllogo/fridaynightfunkin/logo.jpg",
+      },
+      {
+        title: "Getting Over It",
+        url: "getting-over-it.html",
+        image: "https://mathsframe.github.io/alllogo/getting-over-it/logo.jpg",
+      },
+      {
+        title: "Go Ball",
+        url: "go-ball.html",
+        image: "https://mathsframe.github.io/alllogo/go-ball/logo.jpg",
+      },
+      {
+        title: "Golf Battle",
+        url: "golf-battle.html",
+        image: "https://mathsframe.github.io/alllogo/golf-battle/logo.jpeg",
+      },
+      {
+        title: "Moto X3M 2",
+        url: "moto-x3m-2.html",
+        image: "https://mathsframe.github.io/alllogo/moto-x3m-2/logo.png",
+      },
+      {
+        title: "Moto X3M",
+        url: "motox3m.html",
+        image: "https://mathsframe.github.io/alllogo/motox3m/logo.jpg",
+      },
+      {
+        title: "Cannon Basketball 4",
+        url: "cannon-basketball-4.html",
+        image:
+          "https://mathsframe.github.io/alllogo/cannon-basketball-4/logo.jfif",
+      },
+      {
+        title: "Basket and Ball",
+        url: "basket-and-ball.html",
+        image: "https://mathsframe.github.io/alllogo/basket-and-ball/logo.jpg",
+      },
+      {
+        title: "Christmas Fishing IO",
+        url: "christmasfishingio.html",
+        image:
+          "https://mathsframe.github.io/alllogo/christmasfishingio/logo.png",
+      },
+      {
+        title: "Edge Surf",
+        url: "edge-surf.html",
+        image: "https://mathsframe.github.io/alllogo/edge-surf/logo.jpg",
+      },
+      {
+        title: "Fruit Ninja",
+        url: "fruit-ninja.html",
+        image: "https://mathsframe.github.io/alllogo/fruit-ninja/logo.jpg",
+      },
+      {
+        title: "Goal in One",
+        url: "goal-in-one.html",
+        image: "https://mathsframe.github.io/alllogo/goal-in-one/logo.jpeg",
+      },
+      {
+        title: "Mini Golf",
+        url: "mini-golf.html",
+        image: "https://mathsframe.github.io/alllogo/mini-golf/logo.png",
+      },
+      {
+        title: "Soccer Heads",
+        url: "SoccerHeads.html",
+        image: "https://mathsframe.github.io/alllogo/1hls/SoccerHeads/logo.png",
+      },
+      {
+        title: "Soccer Heroes",
+        url: "SoccerHeroes.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/SoccerHeroes/logo.png",
+      },
+      {
+        title: "Moto X3M Pool Party",
+        url: "moto-x3m-pool-party.html",
+        image:
+          "https://mathsframe.github.io/alllogo/1hls/moto-x3m-pool-party/logo.png",
       },
     ];
   },
